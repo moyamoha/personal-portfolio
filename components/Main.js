f6d8444724f10473448edc7c3d252b604b645b9d
moyamoha <82891624+moyamoha@users.noreply.github.com>
2022-03-03 18:59:56 +0200

    update readme and add new project

--- components/Main.js
+++ components/Main.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import TechStack from './TechStack';
+import React from "react";
+import Image from "next/image";
+import TechStack from "./TechStack";
 
 export default function Main() {
-  return (
+	return (
 		<div className="container-fluid d-md-flex px-sm-0 pt-2">
 			<img className="my-img" src="/yahya.jpg" alt="picture of yahya"></img>
-      <section className='w-100'>
-      <p>
-				I am an IT-student at the university of Jyväskylä. Currently I am
-				studying towards bachelor degree, after which I would like to land a job
-				in areas I am interested in mainly data science and application/web
-				developement. I love to code readable and maintainable world-changing
-				scripts. My favourite programming languages are Python and JavaScript, but I am not
-				restricted to them. My other hobbies include reading, watching football
-				and talking to friends.
-			</p>
-      <TechStack></TechStack>
-      </section>
+			<section className="w-100">
+				<p>
+					I am an IT-student at the university of Jyväskylä. Currently I am
+					studying towards bachelor degree, after which I would like to land a
+					job in areas I am interested in mainly data science and
+					application/web developement. I love to code readable and maintainable
+					world-changing scripts. My favourite programming languages are Python
+					and JavaScript, but I am not restricted to them. My other hobbies
+					include reading, watching football and talking to friends.
+				</p>
+				<TechStack></TechStack>
+			</section>
 		</div>
 	);
 }
