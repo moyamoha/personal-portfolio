fce398fd607240d5755d53f979da5dbd0de6c980
moyamoha <82891624+moyamoha@users.noreply.github.com>
2022-03-11 10:41:48 +0200

    remove last comma from techstack

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function generateId() {
-  return Math.random().toString(36).replace(/[^a-z]+/g, '').substring(0, 6);
+	return Math.random()
+		.toString(36)
+		.replace(/[^a-z]+/g, "")
+		.substring(0, 6);
 }
 
-export default function ProjectCard({project}) {
-  return (
+export default function ProjectCard({ project }) {
+	return (
 		<div className="d-flex flex-column justify-content-center py-3 px-3 border w-md-25 border-success project">
-			<h4 className='text-success'>{project.title}</h4>
+			<h4 className="text-success">{project.title}</h4>
 			<p>{project.description}</p>
 			<div>
 				<strong>Tech Stack: </strong>
 				{project.techStack.map((ts) => (
-					<span key={generateId()}>{ts},</span>
+					<span key={generateId()}>{ts}</span>
 				))}
 			</div>
-			<a className="project__demo-link text-primary" href={`${project.linkToDemoOrCodebase}`} target="_blank">Demo/Codebase</a>
+			<a
+				className="project__demo-link text-primary"
+				href={`${project.linkToDemoOrCodebase}`}
+				target="_blank"
+			>
+				Demo/Codebase
+			</a>
 		</div>
 	);
 }
