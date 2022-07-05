3ce524fd30173b0a146bf0aa02e3fc7aad192da1
unknown <salimiyahya549@gmail.com>
2022-07-06 01:57:30 +0300

    edit experience in integrify

--- components/Work.js
+++ components/Work.js
@@ -1,47 +1,54 @@
-import React from 'react'
+import React from "react";
 
 export default function Work() {
-  return (
+	return (
 		<div className="timeline">
 			<div className="workcontainer left">
 				<div className="content">
-        <h2 className='text-info'>1/2022 - present</h2>
-          <h5 className='text-warning'>Full stack developer at Integrify</h5>
+					<h2 className="text-info">1/2022 - 6/2022</h2>
+					<h5 className="text-warning">Full stack developer at Integrify</h5>
 					<p>
-            At integrify we learn as we develop but most importantly we get monitorship from
-             senior developers, so we learn alot from our mistakes.
+						At integrify we learned as we developed but most importantly we
+						received monitorship from senior developers, so we learned alot from
+						our mistakes.
 					</p>
 				</div>
 			</div>
 			<div className="workcontainer right">
 				<div className="content">
-        <h2 className='text-info'>1/2020 - present</h2>
-          <h5 className='text-warning'>Student at University of Jyväskylä</h5>
+					<h2 className="text-info">1/2020 - present</h2>
+					<h5 className="text-warning">Student at University of Jyväskylä</h5>
 					<p>
-            In two and a half year of my academic life, I learned a lot. As information tecknology student we learned basics of programming and how the computer really works.
-            The most important lesson however was how to self-learn. I also liked the math courses except 
-            Discrete maths. Why do everyone is scared of it? 😄
+						In two and a half year of my academic life, I learned a lot. As
+						information tecknology student we learned basics of programming and
+						how the computer really works. The most important lesson however was
+						how to self-learn. I also liked the math courses except Discrete
+						maths. Why do everyone is scared of it? 😄
 					</p>
 				</div>
 			</div>
 			<div className="workcontainer left">
 				<div className="content">
-					<h2 className='text-info'>7/2019 - 8/2019</h2>
-          <h5 className='text-warning'>Assistant at data-management (Ekami) </h5>
+					<h2 className="text-info">7/2019 - 8/2019</h2>
+					<h5 className="text-warning">
+						Assistant at data-management (Ekami){" "}
+					</h5>
 					<p>
-            Ok, it looks short, 
-            but for me it was a fruitful spell. How? I got to do IT-related work for the first time.
-            I was responsible for a variety of things for example installing softwares and basic stuffs 
+						Ok, it looks short, but for me it was a fruitful spell. How? I got
+						to do IT-related work for the first time. I was responsible for a
+						variety of things for example installing softwares and basic stuffs
 					</p>
 				</div>
 			</div>
 			<div className="workcontainer right">
 				<div className="content">
-					<h2 className='text-info'>2017 - 2019</h2>
-          <h5 className='text-warning'>ICT Student at Ekami</h5>
+					<h2 className="text-info">2017 - 2019</h2>
+					<h5 className="text-warning">ICT Student at Ekami</h5>
 					<p>
-            Here begins the main journey. From installing windows to managing windows server 2012, we also learned some basic html and css stuff.
-            At the end of it, I knew I definitely wanted higher education in some University
+						Here begins the main journey. From installing windows to managing
+						windows server 2012, we also learned some basic html and css stuff.
+						At the end of it, I knew I definitely wanted higher education in
+						some University
 					</p>
 				</div>
 			</div>
