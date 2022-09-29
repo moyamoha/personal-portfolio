e07fd328f51b40b8e0d6d5dc5dae16da689bec65
Yahya Salimi <yahya.salimi@eduhouse.fi>
2022-09-29 08:29:57 +0300

    add update

--- pages/projects.js
+++ pages/projects.js
@@ -9,7 +9,8 @@ export default function Projects() {
 				<h1>Projects</h1>
 				<p>
 					I have worked with a couple of projects, which are mostly hobby
-					projects or things I built while learning new technologies
+					projects or things I built while learning new technologies <br></br>
+					<i>Update 9/2022: Most of projects used to have a demo link. They were deployed to heroku, but heroku stops providing free dynos this year, so I removed them from Heroku. I will try to deploy them to other service providers as soon as I can.</i>
 				</p>
 			</div>
 			<div className="container d-flex py-3 px-md-0 proj-container">
