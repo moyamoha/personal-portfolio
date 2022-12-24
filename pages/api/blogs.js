7252db44526b04e9ffb5cf407d9da9338ac725a6
Yahya Salimi <yahya.salimi@eduhouse.fi>
2022-12-24 14:08:04 +0200

    Changes on 2022-12-24 14:08:04.449475

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,6 +1,5 @@
 import fs from "fs/promises";
 import Cors from "cors";
-import { runInNewContext } from "vm";
 
 const cors = Cors({
   methods: ["POST", "GET", "HEAD"],
@@ -25,7 +24,7 @@ export default async function handler(req, res) {
     return;
   }
   try {
-    const fileBuffer = await fs.readFile("./data/blogs.json");
+    const fileBuffer = await fs.readFile("../../data/blogs.json");
     const blogs = JSON.parse(fileBuffer);
     const blogObject = {
       title: req.body.title,
@@ -33,7 +32,7 @@ export default async function handler(req, res) {
       updatedAt: new Date().toISOString(),
     };
     blogs.push(blogObject);
-    await fs.writeFile("./data/blogs.json", JSON.stringify(blogs));
+    await fs.writeFile("../../data/blogs.json", JSON.stringify(blogs));
     res.status(200).end();
   } catch (e) {
     res.json(JSON.stringify(e));
