7ad1012e7bf1d5481171835136044449eeb2f78d
Yahya Salimi <yahya.salimi@eduhouse.fi>
2023-05-05 10:44:04 +0300

    show notif all the time but not in sticky mode

--- components/Notif.js
+++ components/Notif.js
@@ -1,23 +1,7 @@
-import { useEffect, useState } from 'react'
-import { AiOutlineClose } from 'react-icons/ai'
-
 export default function Notif({ msg }) {
-  const [showNotif, setShowNotif] = useState(true)
-  useEffect(() => {
-    if (window) {
-      const showBigUpdate = window.localStorage.getItem('bigUpdate')
-      if (showBigUpdate) setShowNotif(JSON.parse(showBigUpdate))
-    } 
-  }, [setShowNotif])
-
-  const hideNotif = () => {
-    window.localStorage.setItem("bigUpdate", "false")
-    setShowNotif(false)
-  }
   return (
-    showNotif ? <div className="container-fluid d-md-flex px-sm-0 pt-2 notif-snacker">
+    <div className="container-fluid d-md-flex px-sm-0 pt-2 notif-snacker">
       <span>{msg}</span>
-      <AiOutlineClose className="close-notif-icon" onClick={hideNotif}></AiOutlineClose>
-    </div> : <></>
+    </div>
   );
 }
