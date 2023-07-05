502d740e8e4994ced9864d5bdca69af8507b0246
Yahya Salimi <yahya.salimi@eduhouse.fi>
2023-07-05 16:42:05 +0300

    change blogs urls

--- state/data.api.js
+++ state/data.api.js
@@ -3,7 +3,7 @@ import { setBlogs } from "./data.slice";
 export function loadBlogs() {
   return async (dispatch, getState) => {
     try {
-      const res = await fetch("https://s4sqyr.deta.dev/blogs");
+      const res = await fetch("https://blogbackend-1-j2721633.deta.app/blogs");
       const blogs = await res.json();
       localStorage.setItem("blogs", JSON.stringify(blogs));
       dispatch(setBlogs(blogs));
